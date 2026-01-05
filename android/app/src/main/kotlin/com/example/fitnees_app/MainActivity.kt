7df3f3dd87ca2d0a413d4a785837489c4fcc1e45
IGNN3L_Z3R0 <ignnel01end@gmail.com>
2026-01-04 20:00:17 -0500

    deber de plugins final

--- android/app/src/main/kotlin/com/example/fitnees_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/fitnees_app/MainActivity.kt
@@ -3,6 +3,5 @@ package com.tuinstituto.fitness_tracker
 import io.flutter.embedding.android.FlutterFragmentActivity
 
 class MainActivity: FlutterFragmentActivity() {
-    // ¡Ya no necesitamos código aquí! 
-    // Los plugins se configuran automáticamente
+    // Ya no necesitamos codigo aqui ya que los plugins se configuran automaticamente
 }
